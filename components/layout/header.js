2fcceb95248db08c661e682070cb3f63f8b8e024
Vidushi Rastogi <vidu1998rastogi@gmail.com>
2023-02-14 17:19:40 +0530

    Added API handler to get users, fixed search chat parter feature

--- components/layout/header.js
+++ components/layout/header.js
@@ -1,32 +1,14 @@
-import { Row, Col, Popover, Card, Modal, Input, notification, Button } from 'antd';
+import { Row, Col, Popover, Modal } from 'antd';
 import {
     BellFilled,
     SettingFilled,
-    UserAddOutlined,
-    CheckOutlined,
-    CloseOutlined
 } from '@ant-design/icons';
 import styles from './layout.module.css';
 import { useState } from 'react';
 import { signOut } from 'next-auth/react';
+import SearchChatPartner from './searchChatPartner';
 
 const settings = ['Profile Settings', 'Privacy Settings', 'Log Out'];
-const usernames = [
-    'ufuckedup',
-    'toobored',
-    'naruto',
-    'poorhooman',
-    'tomuchstress',
-    'poopyhead',
-    'uunknown',
-    'nanamin',
-    'mikasa',
-    'mimiluv',
-    'memeer',
-    'viitoo',
-    'veeebhav'
-]
-const { Search } = Input;
 
 const notifications = (chatNotifications) => (
     <div className={styles.notificationsDiv}>
@@ -43,14 +25,6 @@ const notifications = (chatNotifications) => (
 export default function PageHeader({ home, chatNotifications, session }) {
     const [selectedSettingOption, setSelectedSettingOption] = useState('');
     const [showLogOutModal, setShowLogOutModal] = useState(false);
-    const [searchedUsername, setSearchedUsername] = useState('');
-    const [matchedUsernames, setMatchedUsernames] = useState([]);
-
-    const popNotification = (type, message) => {
-        notification[type]({
-          message
-        })
-    }
 
     const handleSettingOptionClick = (option) => {
         setSelectedSettingOption(option)
@@ -70,48 +44,6 @@ export default function PageHeader({ home, chatNotifications, session }) {
     const handleCancelLogOut = () => {
         setShowLogOutModal(false);
     };
-
-    const showMatchedUsernames = (value) => {
-        if (value === '') {
-            setMatchedUsernames([])
-        }
-        else {
-            const matches = usernames.filter(username => username.startsWith(value));
-            setMatchedUsernames(matches);
-        }
-    }
-
-    const addChatPartner = async (partnerUsername) => {
-        console.log(`Add ${partnerUsername} to ${session.user.username}`);
-        const res = await fetch('/api/addChatPartner', {
-            method: 'POST',
-            body: JSON.stringify({
-                chatId: `${session.user.username}_${partnerUsername}`,
-                chats: []
-            }),
-            header: {
-                'Content-Type': 'application/json'
-            }
-        });
-        const status = res.status;
-        const response = await res.json()
-        if (status === 200) {
-            popNotification('success', 'This user has been added as your chat partner :)');
-        }
-        else {
-            popNotification('error', 'Something went wrong while adding this user as your chat partner :(')
-        }
-    }
-
-    const handleAddChatParter = (username) => {
-        notification.open({
-            message: `Do you want to send chat request to @${username}?`,
-            description: 
-            <>
-                <Button shape='circle' icon={<CheckOutlined />} onClick={() => addChatPartner(username)}/>
-            </>,
-          });
-    }
 
     const settingOptions = (
         <>
@@ -142,23 +74,8 @@ export default function PageHeader({ home, chatNotifications, session }) {
             <Col span={3} className={styles.header}>
                 <h1>ChatItUp!</h1>
             </Col>
-            {!home ? <Col span={6} style={{paddingTop: '10px'}}>
-                        <Search
-                            placeholder="Search your chat partner"
-                            onSearch={value => showMatchedUsernames(value)}
-                        />
-                {matchedUsernames.length > 0 ?
-                    <div className={styles.searchedCard}>
-                        {matchedUsernames.map(username =>
-                            <Row className={styles.searchedUsername} justify='space-between'>
-                                <p style={{ margin: '0px' }}>@{username}</p>
-                                <Button 
-                                    shape='circle'
-                                    icon={<UserAddOutlined />}
-                                    onClick={() => handleAddChatParter(username)}/>
-                            </Row>
-                        )}
-                    </div> : <></>}
+            {!home ? <Col span={6}>
+                        <SearchChatPartner session={session}/>
                     </Col> : <></>}
             {!home ?
                 <>
---
+++ pages/api/getAllUsers.js
@@ -0,0 +1,25 @@
+import connect from "../../lib/mongodb";
+import User from '../../model/userSchema';
+
+export default async function handler(req, res) {
+    const searchKey = req.query.key;
+    const searchRegex = new RegExp(`^${searchKey}`, 'g');
+    try {
+        connect();
+        try {
+            const users = await User.find({ username: { $regex: searchRegex } })
+            if (!users) {
+                return res.status(404).json({message: 'No user found with this username'});
+            }
+            else {
+                return res.status(200).json({message: 'Users received', users: users});
+            }
+        }
+        catch (error) {
+            return res.status(400).json({message: 'Unable to get users', error});
+        }
+    }
+    catch (error) {
+        return res.status(500).json({message: 'Cannot connect with database'});
+    }
+}
