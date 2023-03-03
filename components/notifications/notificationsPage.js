179b7a410a1584b7485ddc2a616d80e18d16cc4c
Vidushi Rastogi <vidu1998rastogi@gmail.com>
2023-03-03 23:02:30 +0530

    Added UI for notifications page

---
+++ components/notifications/notificationsPage.js
@@ -0,0 +1,98 @@
+import React, { useEffect, useState } from 'react';
+import { Avatar, Button, List, message, Row, Col, Tooltip } from 'antd';
+import VirtualList from 'rc-virtual-list';
+import {
+    UserOutlined,
+    CheckOutlined,
+    CloseOutlined
+} from '@ant-design/icons';
+
+const NotificationDescription = ({ type, notificationType }) => (
+    <>
+        {notificationType === 'chat_request' ?
+            type === 'incoming' ?
+                <p>Sent you a chat request</p>
+                :
+                <p>Received a chat request from you</p>
+        :
+        notificationType === 'request_accepted' ?
+            <p>Accepted your chat request</p>
+        :
+        <></>}
+    </>
+)
+
+const ActionButtons = ({type}) => (
+    <div>
+        {type === 'incoming' ? 
+            <Row justify='space-between'>
+                <Col span={12}>
+                    <Tooltip title='Accept'>
+                        <Button 
+                            style={{borderColor: '#1677ff', color: '#1677ff', marginRight: '10px'}} 
+                            shape='circle'
+                            icon={<CheckOutlined />}/>
+                    </Tooltip>
+                </Col>
+                <Col span={12}>
+                    <Tooltip title='Reject'>
+                        <Button danger shape='circle' icon={<CloseOutlined />}/>
+                    </Tooltip>
+                </Col>
+            </Row>
+            :
+            <Button danger>Cancel</Button>
+        }
+    </div>
+)
+
+export default function NotificationsPage({ type, notifications }) {
+    console.log('TYPE AND NOTIFICATIONS : ', type, notifications)
+    const totalNotifications = notifications.length;
+    const [data, setData] = useState(notifications.slice(0, 5));
+    console.log('data : ', notifications.slice(0, 5))
+    const appendData = () => {
+        if (data.length < totalNotifications) {
+            if (totalNotifications - data.length > 5) {
+                setData([...data, ...notifications.slice(data.length, data.length + 5)]);
+            }
+            else {
+                setData([...data, ...notifications.slice(data.length, data.length + (totalNotifications - data.length))]);
+            }
+        }
+    };
+    // useEffect(() => {
+    //     appendData();
+    // }, []);
+    const onScroll = (e) => {
+        if (e.currentTarget.scrollHeight - e.currentTarget.scrollTop === 350) {
+            appendData();
+        }
+    };
+    return (
+        <List>
+            {console.log('item : ', data)}
+            <VirtualList
+                data={data}
+                height={350}
+                itemHeight={47}
+                itemKey={"_id"}
+                onScroll={onScroll}
+            >
+                {(item) => (
+                    <List.Item key={item._id}>
+                        <List.Item.Meta
+                            avatar=
+                            {<Avatar
+                                //src={item.picture.large} 
+                                icon={<UserOutlined />} />}
+                            description={<NotificationDescription type={type} notificationType={item.type} />}
+                            title={type === 'incoming' ? <p>{item.sender}</p> : <p>{item.receiver}</p>}
+                        />
+                        <ActionButtons type={type}/>
+                    </List.Item>
+                )}
+            </VirtualList>
+        </List>
+    );
+}
--- pages/api/postNotification.js
+++ pages/api/postNotification.js
@@ -2,39 +2,47 @@ import connect from "../../lib/mongodb";
 import Notification from "../../model/notificationSchema";
 
 export default async function handler(req, res) {
-    // const requestBody = JSON.parse(req.body);
     const fromUser = req.body.fromUser;
     const toUser = req.body.toUser;
     const type = req.body.type;
+    const acknowledgement = req.body.acknowledgement;
     const timeStamp = req.body.timeStamp;
+
     const inNotification = {
         sender: fromUser,
         type: type,
-        timeStamp: timeStamp
+        timeStamp: new Date(timeStamp)
     }
+
     const outNotification = {
         receiver: toUser,
         type: type,
-        timeStamp: timeStamp
+        timeStamp: new Date(timeStamp)
     }
+
     try {
         connect();
         try {
             await Notification.updateOne(
                 {username: toUser},
                 {$push: {incomingNotifications: inNotification}}
-            ).then(async (res) => {
-                await Notification.updateOne(
-                    {username: fromUser},
-                    {$push: {outgoingNotifications: outNotification}}
-                )
-                .then((res) => {
-                    return res.status(200).json({ message: `Notification log has been added to ${fromUser} and ${toUser}` });
-                })
+            ).then(async (response) => {
+                if (acknowledgement) {
+                    await Notification.updateOne(
+                        {username: fromUser},
+                        {$push: {outgoingNotifications: outNotification}}
+                    )
+                    .then((response) => {
+                        return res.status(200).json({ message: `Notification log has been added to ${fromUser} and ${toUser}` });
+                    })
+                }
+                else {
+                    return res.status(200).json({message: `Notification has been logged for ${toUser}`})
+                }
             })
         }
         catch(error) {
-            return res.status(400).json({message: 'Error: While adding notification log'});
+            return res.status(400).json({message: 'Error: While adding notification log', error: error});
         }
     }
     catch(error) {
