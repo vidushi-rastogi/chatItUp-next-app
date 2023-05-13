082822cee21b00df89613cc9c81110d680f486f8
Imran Ansari <imransindrisnd@gmail.com>
2023-05-13 13:20:20 -0400

    intial commit

--- components/chat-room/sider.js
+++ components/chat-room/sider.js
@@ -4,7 +4,7 @@ import {
 } from '@ant-design/icons';
 import styles from './chat.module.css';
 
-export default function PageSider({ setCurrentActiveChat, chatPartners}) {
+export default function PageSider({ setCurrentActiveChat, chatPartners, currentActiveChat}) {
 
     const handleCurrentActiveChatChange = (currentActiveChatUsername) => {
         setCurrentActiveChat(currentActiveChatUsername)
@@ -14,13 +14,15 @@ export default function PageSider({ setCurrentActiveChat, chatPartners}) {
         <Card>
             <Row>
                 <Col span={24}>
-                    <h3 style={{marginTop:'0'}}>Your Chats</h3>
+                <span className='font-bold text-lg'>Your Chats</span>
                 </Col>
             </Row>
             {chatPartners.map(partner => (
+            <>
             <Row 
                 className={styles.chatRow}
                 key={partner}
+                style={{ backgroundColor: partner === currentActiveChat && "#bde0ff" }}
                 onClick={() => handleCurrentActiveChatChange(partner)}
             >
                 <Col span={4}>
@@ -30,6 +32,34 @@ export default function PageSider({ setCurrentActiveChat, chatPartners}) {
                     <h4 className={styles.chatUserName}>@{partner}</h4>
                 </Col>
             </Row>
+            <Row 
+            className={styles.chatRow}
+            key={partner}
+            style={{ backgroundColor: partner === currentActiveChat && "#bde0ff" }}
+            onClick={() => handleCurrentActiveChatChange(partner)}
+        >
+            <Col span={4}>
+                <MehTwoTone className={styles.chatUserPhoto}/>
+            </Col>
+            <Col span={20}>
+                <h4 className={styles.chatUserName}>@{partner}</h4>
+            </Col>
+        </Row>
+        <Row 
+                className={styles.chatRow}
+                key={partner}
+                style={{ backgroundColor: partner === currentActiveChat && "#bde0ff" }}
+                onClick={() => handleCurrentActiveChatChange(partner)}
+            >
+                <Col span={4}>
+                    <MehTwoTone className={styles.chatUserPhoto}/>
+                </Col>
+                <Col span={20}>
+                    <h4 className={styles.chatUserName}>@{partner}</h4>
+                </Col>
+            </Row>
+           
+        </>
             ))}
         </Card>
     </div>
